1d1447db61522260091d93b4e2048438a0cde64a
Roope <roope.laakso@gmail.com>
2020-01-08 01:07:41 +0200

    cross-origin annotaatio lisätty

--- src/main/java/hh/swd20/BookReview/web/BookController.java
+++ src/main/java/hh/swd20/BookReview/web/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -90,7 +91,7 @@ public class BookController {
 	
 	// REST 
 	
-	
+	@CrossOrigin
 	@GetMapping("/booksRest")
 	public @ResponseBody List<Book> getBooksRest() {
 		 return (List<Book>) bookRepo.findAll();
